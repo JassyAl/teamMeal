3a578063a18f276816ad261701b1fe48a1109a3a
SHEUN BADMUS <sobadmus@mail.fhsu.edu>
2023-03-20 08:31:10 +0000

    SignIn UI

--- src/components/SignOn/SignIn.jsx
+++ src/components/SignOn/SignIn.jsx
@@ -1,110 +1,158 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React, { useState } from "react";
+import {
+  TextField,
+  Button,
+  Typography,
+  Grid,
+  Box,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
-function Copyright(props) {
-    return (
-        <Typography variant="body2" color="text.secondary" align="center" {...props}>
-            {'Copyright © '}
-            <Link color="inherit" href="https://mui.com/">
-                Your Website
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+    backgroundColor: "#1A1A1A",
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  title: {
+    color: "lightgreen",
+    textAlign: "center",
+    marginBottom: theme.spacing(4),
+  },
+  textField: {
+    "& .MuiOutlinedInput-root": {
+      borderRadius: 30,
+      backgroundColor: "#333333",
+      color: "white",
+    },
+    "& .MuiOutlinedInput-input": {
+      padding: "15.5px 14px",
+    },
+    "& .MuiInputLabel-outlined": {
+      color: "white",
+    },
+    "& .MuiOutlinedInput-notchedOutline": {
+      
+    },
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: "lightgreen",
+    color: "black",
+    borderRadius: 30,
+    "&:hover": {
+      backgroundColor: "green",
+    },
+  },
+  signup: {
+    color: "white",
+    textAlign: "center",
+  },
+}));
+
+export default function Login() {
+  const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // TODO: login logic
+  };
+
+  return (
+    <Grid
+      container
+      direction="column"
+      justify="center"
+      alignItems="center"
+      className={classes.root}
+    >
+      <Box p={5}>
+        <Typography variant="h4" className={classes.title}>
+          Login to your Account
         </Typography>
-    );
-}
-
-const theme = createTheme();
-
-export default function SignIn() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
-    };
-
-    return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="primary" />}
-                            label="Remember me"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Sign In
-                        </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                    Forgot password?
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link href="#" variant="body2">
-                                    {"Don't have an account? Sign Up"}
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Box>
-                <Copyright sx={{ mt: 8, mb: 4 }} />
-            </Container>
-        </ThemeProvider>
-    );
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            className={classes.textField}
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="Enter your Email Address"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            className={classes.textField}
+            value={password}
+            onChange={handlePasswordChange}
+            placeholder="Enter your Password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            className={classes.submit}
+          >
+            Log In
+          </Button>
+        </form>
+        <Box mt={2}>
+          <Typography variant="body2" className={classes.signup}>
+            Not a Member?{" "}
+            <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            href="/signup"
+            //rounded corners
+            style={{ borderRadius: 30 }}
+          >
+            Sign Up
+          </Button>
+          </Typography>
+        </Box>
+        <Box mt={2}>
+          <Typography variant="body2" className={classes.forget}>
+            <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+            href="/forget"
+            //rounded corners
+            style={{ borderRadius: 30 }}
+          >
+            Forget Password
+          </Button>
+          </Typography>
+        </Box>
+      </Box>
+    </Grid>
+  );
 }
