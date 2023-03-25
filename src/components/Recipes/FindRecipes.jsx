ee00f05c7c1ddb26b284aa53e697b17130e8afab
Cheron Fletcher <c_fletcher2@mail.fhsu.edu>
2023-03-25 17:29:20 +0000

    Add Recipe Search

--- src/components/Recipes/FindRecipes.jsx
+++ src/components/Recipes/FindRecipes.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import SearchBar from './SearchBar';
+import DefaultRecipes from "./DefaultRecipes";
+import {fetchFromAPI} from "../../API/fetchFromAPI";
+import { useState, useEffect } from 'react';
+
+
+
 
 function Recipes() {
+
+    const [selectedCategory, setSelectedCategory] = useState('chicken');
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) => setRecipes(data.hits))
+    }, [selectedCategory]);
+
+
   return (
     <div>
-      <h1>Find Recipes</h1>
+      <SearchBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+      <DefaultRecipes recipes={recipes} />
     </div>
   );
 }
