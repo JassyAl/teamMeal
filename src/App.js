6024674b1a3235696a6be18fc40e887937b673b1
BuildTools <unconfigured@null.spigotmc.org>
2023-04-03 00:26:18 -0400

    added initial custom recipe form/page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from "react-router-dom";
 import Landing from "./components/Landing/Landing";
 import SignIn from "./components/SignOn/SignIn";
 import SignUp from "./components/SignOn/SignUp";
@@ -16,17 +16,19 @@ import AccountInfo from "./components/Dashboard/AccountInfo";
 import TAC from "./components/SignOn/TAC";
 import Calendar from "./components/Calendar/Calendar";
 import SavedRecipes from "./components/Recipes/SavedRecipes";
+import CustomRecipes from "./components/Recipes/CustomRecipes";
 
 import { fetchFromAPI } from "./API/fetchFromAPI";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function App() {
-
-  const [selectedCategory, setSelectedCategory] = useState('chicken');
+  const [selectedCategory, setSelectedCategory] = useState("chicken");
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) => setRecipes(data.hits))
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setRecipes(data.hits)
+    );
   }, [selectedCategory]);
 
   const location = useLocation();
@@ -41,21 +43,33 @@ function App() {
         <Route path="/SignUp" element={<SignUp />}></Route>
 
         <Route path="FindRecipes">
-          <Route index element={<FindRecipes selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} recipes={recipes} />}></Route>
-          <Route path=":label" element={<RecipePage recipes={recipes} />}></Route>
+          <Route
+            index
+            element={
+              <FindRecipes
+                selectedCategory={selectedCategory}
+                setSelectedCategory={setSelectedCategory}
+                recipes={recipes}
+              />
+            }
+          ></Route>
+          <Route
+            path=":label"
+            element={<RecipePage recipes={recipes} />}
+          ></Route>
         </Route>
-        <Route path="/SavedRecipes" element={<SavedRecipes/>}></Route>
+        <Route path="/CustomRecipes" element={<CustomRecipes />}></Route>
+        <Route path="/SavedRecipes" element={<SavedRecipes />}></Route>
         <Route path="/HomePage" element={<HomePage />}></Route>
         <Route path="/Journal" element={<Journal />}></Route>
         <Route path="/DailyProgress" element={<DailyProgress />}></Route>
         <Route path="/Profile" element={<Profile />}></Route>
-        <Route path="/Calendar" element={<Calendar/>}></Route>
+        <Route path="/Calendar" element={<Calendar />}></Route>
         <Route path="/Messages" element={<Messages />}></Route>
         <Route path="/Account" element={<AccountInfo />}></Route>
         <Route path="/Terms" element={<TAC />}></Route>
         <Route path="*" element={<NoPage />} />
       </Routes>
-
     </div>
   );
 }
