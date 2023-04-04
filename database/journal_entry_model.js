2a4670e3ced2be9fb7e57adc1eb939483e40b80d
gaguerragonzalez <128244543+gaguerragonzalez@users.noreply.github.com>
2023-04-04 14:19:59 -0500

    save/POST journals, moved corresponding api endpoint to /journals

--- database/journal_entry_model.js
+++ database/journal_entry_model.js
@@ -2,7 +2,7 @@ var pool = require('./Database');
 
 const getEntries = () => {
   return new Promise(function(resolve, reject) {
-    pool.query('SELECT journal_id AS id, journal_date AS date, title, journal_entry AS content FROM journals ORDER BY journal_date DESC', (error, results) => {
+    pool.query('SELECT journal_id AS id, journal_date AS date, title, journal_entry AS content FROM journals ORDER BY journal_date DESC, id DESC', (error, results) => {
       if (error) {
         console.log(error);
         reject(error);
@@ -15,17 +15,18 @@ const getEntries = () => {
 const createEntry = (body) => {
   return new Promise(function(resolve, reject) {
     const { date, title, entry } = body;
-
-    pool.query('INSERT INTO journals (journal_date, title, journal_entry) VALUES ($1, $2, $3) RETURNING *', [date, title, entry], (error, results) => {
+    
+    // not sure what to do with the last 3 columns for now
+    pool.query('INSERT INTO journals (journal_date, title, journal_entry, account_id, category_id, cell_num) VALUES ($1, $2, $3, 1234, 5678, 9012)', [date, title, entry], (error, results) => {
       if (error) {
         reject(error);
       }
-      resolve(`A new entry has been added: ${JSON.stringify(results.rows[0])}`);
+      //resolve(`A new entry has been added: ${JSON.stringify(results.rows[0])}`);
     })
   })
 }
 
-const deleteEntry = (journalId) => {
+/*const deleteEntry = (journalId) => {
   return new Promise(function(resolve, reject) {
     const id = parseInt(journalId);
 
@@ -36,10 +37,10 @@ const deleteEntry = (journalId) => {
       resolve(`journal deleted with ID: ${id}`);
     })
   })
-}
+}*/
 
 module.exports = {
   getEntries,
   createEntry,
-  deleteEntry,
+  //deleteEntry,
 };
